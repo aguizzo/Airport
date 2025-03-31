d0f8d5a9bd70d467a97ec56a45b90f333f1ba0b0
aguizzo <andres.guizzo@gmail.com>
2025-03-31 11:56:28 +0200

    feat: Crud Table for Airport

--- AirportFullStack/src/main/frontend/components/airport/AirportList.tsx
+++ AirportFullStack/src/main/frontend/components/airport/AirportList.tsx
@@ -1,4 +1,15 @@
-import { Typography } from "@mui/material";
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography
+} from "@mui/material";
+
 import { useState, useEffect } from "react";
 
 import { AirportService } from 'Frontend/generated/endpoints';
@@ -16,9 +27,50 @@ const AirportList = () => {
      },[]);
 
     return(
+        <>
         <Typography variant="h1" align="center">
-           Number of Airports: {airports.length}
+            Airport List
         </Typography>
+        <Button
+                variant="contained"
+                color="primary"
+                onClick={() => console.log("create")}
+                style={{ marginBottom: "20px" }}
+        >
+            Create Airport
+        </Button>
+        <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Code</TableCell>
+                  <TableCell>City</TableCell>
+                  <TableCell>Country</TableCell>
+                  <TableCell>Actions</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {airports.map((airport) => (
+                  <TableRow key={airport.id}>
+                    <TableCell>{airport.name}</TableCell>
+                    <TableCell>{airport.code}</TableCell>
+                    <TableCell>{airport.city}</TableCell>
+                    <TableCell>{airport.country}</TableCell>
+                    <TableCell>
+                      <Button color="primary" onClick={() => console.log("update")}>
+                        Update
+                      </Button>
+                      <Button color="secondary" onClick={() => console.log("delete")}>
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+        </TableContainer>
+        </>
     )
 }
 
